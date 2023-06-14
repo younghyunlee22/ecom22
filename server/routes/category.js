90ee184b3328c4064bb501f62220c9bf2dead0b3
younghyunlee22 <younghyunlee22@gmail.com>
2023-06-14 00:20:00 -0500

    CRUD

--- server/routes/category.js
+++ server/routes/category.js
@@ -5,8 +5,15 @@ const router = express.Router();
 // middlewares
 import { requireSignin, isAdmin } from "../middlewares/auth.js";
 //controllers
-import { create } from "../controllers/caterory.js";
+import { create, update, remove, list, read } from "../controllers/category.js";
 
+// /: is for route param, categoryId is an arbitary name for the id of the category
+// /:categoryId - you can choose a category by id
 router.post("/category", requireSignin, isAdmin, create);
+router.put("/category/:categoryId", requireSignin, isAdmin, update);
+router.delete("/category/:categoryId", requireSignin, isAdmin, remove);
+router.get("/categories", list);
+// get a single category
+router.get("/category/:slug", read);
 
 export default router;
