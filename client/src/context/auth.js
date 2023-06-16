ee2300df0482718e274cb8b42f9ed5bb7d215b7e
younghyunlee22 <younghyunlee22@gmail.com>
2023-06-16 00:22:48 -0500

    context

---
+++ client/src/context/auth.js
@@ -0,0 +1,21 @@
+import { useState, createContext, useContext } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({
+    user: null,
+    token: "",
+  });
+
+  return (
+    <AuthContext.Provider value={[auth, setAuth]}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => useContext(AuthContext);
+// const [auth, setAuth] = useAuth()
+
+export { useAuth, AuthProvider };
